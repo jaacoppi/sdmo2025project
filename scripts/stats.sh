db27064edee26aa1f81042dc2e37b00f066f477c
Juho Hiltunen <jhiltune18@student.oulu.fi>
2025-10-18 06:40:33 +0300

    scripts refactored

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -6,49 +6,53 @@
 
 FILE=$1
 
+amounts_ratios() {
+TYPE=$1
+grep ",$TYPE$" $FILE \
+| awk -F, '{t=f=0; for(i=1;i<=NF;i++){if($i=="True")t++; else if($i=="False")f++} print "Trues:",t,"Falses:",f}' \
+| sort | uniq -c \
+| awk '{count[$2" "$3" "$4" "$5]=$1; total+=$1} END{for(k in count) printf "%s %d (%.2f%%)\n", k, count[k], count[k]/total*100}'
+}
+
+criteria_type_amounts() {
+TYPE=$1
+grep ",$TYPE$" $FILE | awk -F, '{for(i=9;i<=12;i++){if($i=="True")t[i]++; else if($i=="False")f[i]++}} END{for(i=9;i<=12;i++) printf "C%d → Trues: %d Falses: %d\n", i-5, t[i], f[i]}'
+
+}
+
+pattern_counts() {
+TYPE=$1
+grep ",$TYPE$" $FILE |  awk -F, '{
+  pattern = $9","$10","$11","$12
+  count[pattern]++
+}
+END{
+  for(p in count) 
+    print count[p], p
+}' | sort -nr
+}
 echo "# c4-c7 True/False amounts and ratios"
 echo "## False positives"
-grep ",FP$" $1 \
-| awk -F, '{t=f=0; for(i=1;i<=NF;i++){if($i=="True")t++; else if($i=="False")f++} print "Trues:",t,"Falses:",f}' \
-| sort | uniq -c \
-| awk '{count[$2" "$3" "$4" "$5]=$1; total+=$1} END{for(k in count) printf "%s %d (%.2f%%)\n", k, count[k], count[k]/total*100}'
+amounts_ratios "FP"
 echo ""
 echo "## True positives"
-grep ",TP$" $1 \
-| awk -F, '{t=f=0; for(i=1;i<=NF;i++){if($i=="True")t++; else if($i=="False")f++} print "Trues:",t,"Falses:",f}' \
-| sort | uniq -c \
-| awk '{count[$2" "$3" "$4" "$5]=$1; total+=$1} END{for(k in count) printf "%s %d (%.2f%%)\n", k, count[k], count[k]/total*100}'
+amounts_ratios "TP"
 
 echo ""
 echo ""
 
 echo "# Which criteria C4-C7 are most often true/false in the sample?"
 echo "## False positives"
-grep ",FP$" $FILE | awk -F, '{for(i=9;i<=12;i++){if($i=="True")t[i]++; else if($i=="False")f[i]++}} END{for(i=9;i<=12;i++) printf "C%d → Trues: %d Falses: %d\n", i-5, t[i], f[i]}'
+criteria_type_amounts "FP"
 echo ""
 echo "## True positives"
-grep ",TP$" $FILE | awk -F, '{for(i=9;i<=12;i++){if($i=="True")t[i]++; else if($i=="False")f[i]++}} END{for(i=9;i<=12;i++) printf "C%d → Trues: %d Falses: %d\n", i-5, t[i], f[i]}'
+criteria_type_amounts "TP"
 
 echo ""
 echo ""
 echo "# Which patters are most common for False / True positive?"
 echo "## False positives"
-grep ",FP$" $FILE |  awk -F, '{
-  pattern = $9","$10","$11","$12
-  count[pattern]++
-}
-END{
-  for(p in count) 
-    print count[p], p
-}' | sort -nr
-
+pattern_counts "FP"
 echo ""
 echo "## True positives"
-grep ",TP$" $FILE |  awk -F, '{
-  pattern = $9","$10","$11","$12
-  count[pattern]++
-}
-END{
-  for(p in count) 
-    print count[p], p
-}' | sort -nr
+pattern_counts "TP"
