2e8bc10f83f29e314631507a3a77f60a6f128f3d
Joel Sundström <joesunds@gmail.com>
2025-10-29 20:13:55 +0200

    Refactored code into functions

--- project1developers.py
+++ project1developers.py
@@ -34,26 +34,18 @@ def create_arguments():
 
     return parser.parse_args()
 
+def read_devs(input_file):
+    # This block of code reads an existing csv of developers
+    DEVS = []
+    # Read csv file with name,dev columns
+    with open(input_file, 'r', newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            DEVS.append(row)
+    # First element is header, skip
+    DEVS = DEVS[1:]
 
-args = create_arguments()
-
-# Get file names from command line arguments
-input_file = args.input
-output_file = args.output
-threshold = args.threshold
-minimum_trues= args.minimum_true_count
-true_columns = [c.strip() for c in args.criteria.split(",")]
-
-# This block of code reads an existing csv of developers
-DEVS = []
-# Read csv file with name,dev columns
-with open(input_file, 'r', newline='', encoding='utf-8') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        DEVS.append(row)
-# First element is header, skip
-DEVS = DEVS[1:]
-
+    return DEVS
 
 # Function for pre-processing each name,email
 def process(dev):
@@ -93,66 +85,92 @@ def process(dev):
 
     return name, first, last, i_first, i_last, email, prefix
 
-
 # Compute similarity between all possible pairs
-SIMILARITY = []
-for dev_a, dev_b in combinations(DEVS, 2):
-    # Pre-process both developers
-    name_a, first_a, last_a, i_first_a, i_last_a, email_a, prefix_a = process(dev_a)
-    name_b, first_b, last_b, i_first_b, i_last_b, email_b, prefix_b = process(dev_b)
+def compute_similarity(DEVS):
+    SIMILARITY = []
+    for dev_a, dev_b in combinations(DEVS, 2):
+        # Pre-process both developers
+        name_a, first_a, last_a, i_first_a, i_last_a, email_a, prefix_a = process(dev_a)
+        name_b, first_b, last_b, i_first_b, i_last_b, email_b, prefix_b = process(dev_b)
 
-    # Conditions of Bird heuristic
-    c1 = sim(name_a, name_b)
-    c2 = sim(prefix_b, prefix_a)
-    c31 = sim(first_a, first_b)
-    c32 = sim(last_a, last_b)
-    c4 = c5 = c6 = c7 = c8 = False
-    # Since lastname and initials can be empty, perform appropriate checks
-    if i_first_a != "" and last_a != "":
-        c4 = i_first_a in prefix_b and last_a in prefix_b
-    if i_last_a != "":
-        c5 = i_last_a in prefix_b and first_a in prefix_b
-    if i_first_b != "" and last_b != "":
-        c6 = i_first_b in prefix_a and last_b in prefix_a
-    if i_last_b != "":
-        c7 = i_last_b in prefix_a and first_b in prefix_a
-    if email_a == email_b:
-        c8 = True
+        # Conditions of Bird heuristic
+        c1 = sim(name_a, name_b)
+        c2 = sim(prefix_b, prefix_a)
+        c31 = sim(first_a, first_b)
+        c32 = sim(last_a, last_b)
+        c4 = c5 = c6 = c7 = c8 = False
+        # Since lastname and initials can be empty, perform appropriate checks
+        if i_first_a != "" and last_a != "":
+            c4 = i_first_a in prefix_b and last_a in prefix_b
+        if i_last_a != "":
+            c5 = i_last_a in prefix_b and first_a in prefix_b
+        if i_first_b != "" and last_b != "":
+            c6 = i_first_b in prefix_a and last_b in prefix_a
+        if i_last_b != "":
+            c7 = i_last_b in prefix_a and first_b in prefix_a
+        if email_a == email_b:
+            c8 = True
 
-    # Save similarity data for each conditions. Original names are saved
-    SIMILARITY.append([dev_a[0], email_a, dev_b[0], email_b, c1, c2, c31, c32, c4, c5, c6, c7, c8])
+        # Save similarity data for each conditions. Original names are saved
+        SIMILARITY.append([dev_a[0], email_a, dev_b[0], email_b, c1, c2, c31, c32, c4, c5, c6, c7, c8])
 
-
+    return SIMILARITY
 
 # Save data on all pairs (might be too big -> comment out to avoid)
-cols = ["name_1", "email_1", "name_2", "email_2", "c1", "c2",
-        "c3.1", "c3.2", "c4", "c5", "c6", "c7", "c8"]
-df = pd.DataFrame(SIMILARITY, columns=cols)
-df.to_csv("devs_similarity.csv", index=False, header=True)
+def save_data_on_all_pairs(SIMILARITY):
+    cols = ["name_1", "email_1", "name_2", "email_2", "c1", "c2",
+            "c3.1", "c3.2", "c4", "c5", "c6", "c7", "c8"]
+    df = pd.DataFrame(SIMILARITY, columns=cols)
+    df.to_csv("devs_similarity.csv", index=False, header=True)
 
+    return df
 
 # Set similarity threshold, check c1-c3 against the threshold
-print("Threshold:", threshold)
-df["c1_check"] = df["c1"] >= threshold
-df["c2_check"] = df["c2"] >= threshold
-df["c3_check"] = (df["c3.1"] >= threshold) & (df["c3.2"] >= threshold)
-df = select_criteria(df, minimum_trues, true_columns)
+def set_similarity(df, minimum_trues, true_columns):
+    print("Threshold:", threshold)
+    df["c1_check"] = df["c1"] >= threshold
+    df["c2_check"] = df["c2"] >= threshold
+    df["c3_check"] = (df["c3.1"] >= threshold) & (df["c3.2"] >= threshold)
+    df = select_criteria(df, minimum_trues, true_columns)
+
+    return df
 
 # Omit "check" columns, save to csv
-df = df[["name_1", "email_1", "name_2", "email_2", "c1", "c2",
+def omit_check_and_save(df, output_file):
+    df = df[["name_1", "email_1", "name_2", "email_2", "c1", "c2",
         "c3.1", "c3.2", "c4", "c5", "c6", "c7", "c8"]]
-df.to_csv(os.path.join(output_file), index=False, header=True)
+    df.to_csv(os.path.join(output_file), index=False, header=True)
 
-# Optional: post-processing sample or interval
-if args.sample_size:
-    sampled_df = df.sample(n=args.sample_size, random_state=42)
-    sampled_file = os.path.join("project1devs", f"devs_similarity_sampled_{args.sample_size}.csv")
-    sampled_df.to_csv(sampled_file, index=False, header=True)
-    print(f"Random sample of {args.sample_size} rows saved to {sampled_file}")
+    return df
 
-elif args.interval:
-    interval_df = df.iloc[::args.interval]
-    interval_file = os.path.join("project1devs", f"devs_similarity_every_{args.interval}th.csv")
-    interval_df.to_csv(interval_file, index=False, header=True)
-    print(f"Every {args.interval}th row saved to {interval_file}")
+#Post-processing sample or interval
+def post_process(df):
+    if args.sample_size:
+        sampled_df = df.sample(n=args.sample_size, random_state=42)
+        sampled_file = os.path.join("project1devs", f"devs_similarity_sampled_{args.sample_size}.csv")
+        sampled_df.to_csv(sampled_file, index=False, header=True)
+        print(f"Random sample of {args.sample_size} rows saved to {sampled_file}")
 
+    elif args.interval:
+        interval_df = df.iloc[::args.interval]
+        interval_file = os.path.join("project1devs", f"devs_similarity_every_{args.interval}th.csv")
+        interval_df.to_csv(interval_file, index=False, header=True)
+        print(f"Every {args.interval}th row saved to {interval_file}")
+    
+# MAIN
+args = create_arguments()
+
+# Get file names from command line arguments
+input_file = args.input
+output_file = args.output
+threshold = args.threshold
+minimum_trues= args.minimum_true_count
+true_columns = [c.strip() for c in args.criteria.split(",")]
+
+# Run everything
+developers = read_devs(input_file)
+similarity = compute_similarity(developers)
+df_all_pairs = save_data_on_all_pairs(similarity)
+df_with_threshold_check = set_similarity(df_all_pairs, minimum_trues, true_columns)
+df_final = omit_check_and_save(df_with_threshold_check, output_file)
+post_process(df_final)
