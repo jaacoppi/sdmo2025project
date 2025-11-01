66f381d9d46bd546e7ff1fa795546ef495938f23
Juho Hiltunen <jhiltune18@student.oulu.fi>
2025-11-01 11:42:15 +0200

    tests: read inputs from an existing file

--- tests/test_project1developers.py
+++ tests/test_project1developers.py
@@ -1,35 +1,25 @@
 import pytest
 import csv
+from pathlib import Path
+
 from project1developers import read_devs
 
-"Creates sample csv with names and emails for tests"
-@pytest.fixture
-def sample_csv(tmp_path):
-    file_path = tmp_path / "devs.csv"
-    with open(file_path, "w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["name", "email"])
-        writer.writerow(["erkki peltonen", "erkki.peltonen@gmail.com"])
-        writer.writerow(["jakub", "jakub@school.warsaw.pl"])
-        writer.writerow(["santeri5473", "5473@gratud.com"])
-        writer.writerow(["päivi pölkynen", "päivi.pölkkynen@users.noreply.github.com"])
-    return file_path
+TESTS_DIR = Path(__file__).parent
 
-"Ensure read_devs reads all rows and skips the header"
-def test_read_devs_return(sample_csv):
-    result = read_devs(sample_csv)
-    exception = [
+def test_read_devs_return():
+    """ Ensure read_devs reads all rows and skips the header """
+    sample_csv = TESTS_DIR / "read_devs_input.csv" 
+    actual = read_devs(sample_csv)
+    expected = [
         ["erkki peltonen", "erkki.peltonen@gmail.com"],
         ["jakub", "jakub@school.warsaw.pl"],
         ["santeri5473", "5473@gratud.com"],
         ["päivi pölkynen", "päivi.pölkkynen@users.noreply.github.com"]
     ]
-    assert result == exception
+    assert actual == expected
 
-"Ensure function handles a CSV with only the header"
-def test_read_devs_empty_file(tmp_path):
-    empty_file = tmp_path / "empty.csv"
-    with open(empty_file, "w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["name", "email"])
-    assert read_devs(empty_file) == []
+def test_read_devs_empty_file():
+    """ Ensure function handles a CSV with only the header"""
+    empty_csv = TESTS_DIR / "read_devs_onlyheader.csv"
+    actual = read_devs(empty_csv)
+    assert actual == []
