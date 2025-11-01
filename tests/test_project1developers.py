68d59b6584f4c8dd8ca92dd146fdd8879c7c5494
Juho Hiltunen <jhiltune18@student.oulu.fi>
2025-11-01 13:24:42 +0200

    tests: integration test

--- tests/test_project1developers.py
+++ tests/test_project1developers.py
@@ -1,9 +1,9 @@
 import pytest
 import csv
+import pandas as pd
 from pathlib import Path
 
-from project1developers import read_devs
-from project1developers import process
+from project1developers import read_devs, process, select_criteria
 
 TESTS_DIR = Path(__file__).parent
 
@@ -50,3 +50,26 @@ def test_process():
     assert prefix2 == "matti.virtanen"
 
    
+
+def test_select_criteria():
+    """ Test that correct rows are filtered out """
+    # Create a test dataframe
+    df = pd.DataFrame([
+        # row 0: All true, should always pass
+        {"c1": 1.0, "c2": 1.0, "c3": 1.0, "c1_check": True, "c2_check": True, "c3_check": True, "c4": True, "c5": True, "c6": True, "c7": True, "c8": True},
+        # row 1: c8 is True, should always pass
+        {"c1": 0.0, "c2": 0.0, "c3": 0.0, "c1_check": False, "c2_check": False, "c3_check": False, "c4": False, "c5": False, "c6": False, "c7": False, "c8": True},
+        # row 2: c1 and c2 are 1.0, should always pass
+        {"c1": 1.0, "c2": 1.0, "c3": 0.0, "c1_check": True, "c2_check": True, "c3_check": False, "c4": False, "c5": False, "c6": False, "c7": False, "c8": False},
+        # row 3: Only two trues, should fail
+        {"c1": 0.4, "c2": 0.4, "c3": 0.4, "c1_check": False, "c2_check": False, "c3_check": False, "c4": True, "c5": True, "c6": False, "c7": False, "c8": False}
+    ])
+
+    true_columns = ["c1_check", "c2_check", "c3_check", "c4", "c5", "c6", "c7", "c8"]
+    minimum_trues = 3
+
+    actual = select_criteria(df, minimum_trues, true_columns)
+    expected = df.iloc[[0,1,2]]
+    pd.testing.assert_frame_equal(actual, expected)
+
+
---
+++ tests/test_project1developers_integration.py
@@ -0,0 +1,29 @@
+import subprocess
+from pathlib import Path
+import pytest
+import pandas as pd
+
+def test_program_creates_correct_output(tmp_path):
+    """ Integration test to see that the program produces correct outputs """
+    TESTS_DIR = Path(__file__).parent
+    input_file = TESTS_DIR / "integration_input.csv"
+    expected_output_file = TESTS_DIR  / "integration_expected_output.csv"
+    actual_output_file = tmp_path / "integration_test_output.csv"
+
+    cmd = [
+        "python3", "project1developers.py",
+        "-i", str(input_file),
+        "-o", str(actual_output_file),
+        "-t", "0.5",
+        "-m", "2"
+    ]
+
+    result = subprocess.run(cmd, capture_output=True, text=True)
+
+    assert result.returncode == 0, f"Program failed:\n{result.stderr}"
+    assert actual_output_file.exists(), "Expected output file was not created"
+
+    df_actual = pd.read_csv(actual_output_file)
+    df_expected = pd.read_csv(expected_output_file)
+    pd.testing.assert_frame_equal(df_actual, df_expected)
+
